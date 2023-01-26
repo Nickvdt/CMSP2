5bf90dfa871a10e398b387813140231b0ba7d258
Ma-Frits <90691243+Ma-Frits@users.noreply.github.com>
2023-01-26 12:40:16 +0100

    test

--- Public/editor.php
+++ Public/editor.php
@@ -4,45 +4,44 @@ require_once('../Private/functions.php');
 require_once('../Private/database.php');
 
 if(isset($_POST['submit'])) {
-    // Get the file and its properties
+
     $file = $_FILES['image'];
     $fileName = $file['name'];
-    $fileTmpName = $file['tmp_name'];
+
     $fileSize = $file['size'];
     $fileError = $file['error'];
     $fileType = $file['type'];
 
-    // Get the file extension
     $fileExt = explode('.', $fileName);
     $fileActualExt = strtolower(end($fileExt));
 
-    // Allowed file types
+
     $allowed = array('jpg', 'jpeg', 'png', 'gif');
 
-    // Check if the file type is allowed
+
     if(in_array($fileActualExt, $allowed)) {
-        // Check for errors
+
         if($fileError === 0) {
-            // Check for file size
+      
             if($fileSize < 1000000) {
-                // Give the file a new unique name
+      
                 $fileNameNew = uniqid('', true).".".$fileActualExt;
-                // Set the destination for the file
+              
                 $fileDestination = 'uploads/'.$fileNameNew;
-                // Move the file from the temporary location to the destination
+              
                 move_uploaded_file($fileTmpName, $fileDestination);
 
                 $username = $_POST['username'];
                 $password = $_POST['password'];
                 $user_type = $_POST['user_type'];
-                // Prepare the SQL statement
+         
                 $stmt = $db->prepare("INSERT INTO users (username, password, user_type, image) VALUES (:username, :password, :user_type, :image)");
-                // Bind the parameters
+           
                 $stmt->bindParam(':username', $username);
                 $stmt->bindParam(':password', $password);
                 $stmt->bindParam(':user_type', $user_type);
                 $stmt->bindParam(':image', $fileDestination);
-// Execute the statement
+
 $stmt->execute();
 if ($db->changes() > 0) {
     echo "Afbeelding is succesvol opgeslagen in de database";
@@ -72,8 +71,7 @@ echo "Dit bestandstype is niet toegestaan";
 <body>
 <form action="" method="post" enctype="multipart/form-data">
     <input type="file" name="image" required>
-    <input type="text" name="username" required>
-    <input type="password" name="password" required>
+
     <select name="user_type" required>
         <option value="">Selecteer een optie</option>
         <option value="user">Gebruiker</option>
