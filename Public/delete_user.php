15f748405912a2c7db7ffd805773dc91b4afc25c
Daniel <33257@ma-web.nl>
2023-04-15 20:15:10 +0200

    OOP

--- Public/delete_user.php
+++ Public/delete_user.php
@@ -3,6 +3,26 @@ require_once('../Private/init.php');
 require_once('../Private/functions.php');
 require_once('../Private/database.php');
 
+class User {
+    private $db;
+
+    public function __construct($database) {
+        $this->db = $database;
+    }
+
+    public function delete($id) {
+        $query = "DELETE FROM users WHERE id = :id";
+        $stmt = $this->db->prepare($query);
+        $stmt->bindParam(':id', $id);
+
+        if ($stmt->execute()) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
+
 // check if user id is passed in the URL
 if (!isset($_GET['id']) || !is_numeric($_GET['id'])) {
     header('location: admin.php');
@@ -17,12 +37,9 @@ if (is_logged_in()==false) {
 
 $id = $_GET['id'];
 
-// delete user from the database
-$query = "DELETE FROM users WHERE id = :id";
-$stmt = $db->prepare($query);
-$stmt->bindParam(':id', $id);
+$user = new User($db);
 
-if ($stmt->execute()) {
+if ($user->delete($id)) {
     header('location: admin.php');
     exit;
 } else {
