433d8dcec9a97bacdc654a6901686acf1598aa7c
Ma-Frits <90691243+Ma-Frits@users.noreply.github.com>
2023-04-18 09:13:35 +0200

    Editor.php naar OOP

--- Public/editor.php
+++ Public/editor.php
@@ -3,61 +3,80 @@ require_once('../Private/init.php');
 require_once('../Private/functions.php');
 require_once('../Private/database.php');
 
-if(isset($_POST['submit'])) {
+class User {
+    private $username;
+    private $password;
+    private $user_type;
+    private $image;
 
-    $file = $_FILES['image'];
-    $fileName = $file['name'];
-    $fileSize = $file['size'];
-    $fileError = $file['error'];
-    $fileType = $file['type'];
+    public function __construct($username, $password, $user_type, $image) {
+        $this->username = $username;
+        $this->password = $password;
+        $this->user_type = $user_type;
+        $this->image = $image;
+    }
 
+    public function save() {
+        $db = new Database(); // assuming Database class exists and creates a database connection
 
-    $fileExt = explode('.', $fileName);
-    $fileActualExt = strtolower(end($fileExt));
+        $stmt = $db->prepare("INSERT INTO users (username, password, user_type, image) VALUES (:username, :password, :user_type, :image)");
 
+        $stmt->bindParam(':username', $this->username);
+        $stmt->bindParam(':password', $this->password);
+        $stmt->bindParam(':user_type', $this->user_type);
+        $stmt->bindParam(':image', $this->image);
 
-    $allowed = array('jpg', 'jpeg', 'png', 'gif');
-
-
-    if(in_array($fileActualExt, $allowed)) {
-
-        if($fileError === 0) {
-   
-            if($fileSize < 1000000) {
-
-                $fileNameNew = uniqid('', true).".".$fileActualExt;
- 
-                $fileDestination = 'uploads/'.$fileNameNew;
-
-                move_uploaded_file($fileTmpName, $fileDestination);
-
-                $username = $_POST['username'];
-                $password = $_POST['password'];
-                $user_type = $_POST['user_type'];
-
-                $stmt = $db->prepare("INSERT INTO users (username, password, user_type, image) VALUES (:username, :password, :user_type, :image)");
-
-                $stmt->bindParam(':username', $username);
-                $stmt->bindParam(':password', $password);
-                $stmt->bindParam(':user_type', $user_type);
-                $stmt->bindParam(':image', $fileDestination);
-
-$stmt->execute();
-if ($db->changes() > 0) {
-    echo "Afbeelding is succesvol opgeslagen in de database";
-} else {
-    echo "Er is iets misgegaan tijdens het opslaan van de afbeelding";
+        $stmt->execute();
+        if ($db->changes() > 0) {
+            echo "Afbeelding is succesvol opgeslagen in de database";
+        } else {
+            echo "Er is iets misgegaan tijdens het opslaan van de afbeelding";
+        }
+    }
 }
-} else {
-echo "Het bestand is te groot";
+
+class ImageUploader {
+    private $allowedExtensions = array('jpg', 'jpeg', 'png', 'gif');
+
+    public function upload() {
+        if(isset($_POST['submit'])) {
+            $file = $_FILES['image'];
+            $fileName = $file['name'];
+            $fileSize = $file['size'];
+            $fileError = $file['error'];
+            $fileType = $file['type'];
+
+            $fileExt = explode('.', $fileName);
+            $fileActualExt = strtolower(end($fileExt));
+
+            if(in_array($fileActualExt, $this->allowedExtensions)) {
+                if($fileError === 0) {
+                    if($fileSize < 1000000) {
+                        $fileNameNew = uniqid('', true).".".$fileActualExt;
+                        $fileDestination = 'uploads/'.$fileNameNew;
+                        move_uploaded_file($file['tmp_name'], $fileDestination);
+
+                        $username = $_POST['username'];
+                        $password = $_POST['password'];
+                        $user_type = $_POST['user_type'];
+
+                        $user = new User($username, $password, $user_type, $fileDestination);
+                        $user->save();
+                    } else {
+                        echo "Het bestand is te groot";
+                    }
+                } else {
+                    echo "Er is een fout opgetreden tijdens het uploaden van het bestand";
+                }
+            } else {
+                echo "Dit bestandstype is niet toegestaan";
+            }
+        }
+    }
 }
-} else {
-echo "Er is een fout opgetreden tijdens het uploaden van het bestand";
-}
-} else {
-echo "Dit bestandstype is niet toegestaan";
-}
-}
+
+$imageUploader = new ImageUploader();
+$imageUploader->upload();
 ?>
 <!DOCTYPE html>
 <html lang="en">
